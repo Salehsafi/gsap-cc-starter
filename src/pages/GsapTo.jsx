ce1f0d4f4cbf907071fb5b9137d67d7201f1de3c
Salehsafi <107122794+Salehsafi@users.noreply.github.com>
2024-12-18 23:21:31 -0500

    edits

--- src/pages/GsapTo.jsx
+++ src/pages/GsapTo.jsx
@@ -1,6 +1,14 @@
+import { useGSAP } from "@gsap/react";
+
 const GsapTo = () => {
   // TODO: Implement the gsap.to() method
-
+useGSAP(() => {
+  gsap.to('#blue-box', {
+    x: 250,
+    repeat: -1,
+    yoyo: true,
+  })
+});
   return (
     <main>
       <h1>GsapTo</h1>
